d316cecefb5726a6133197c090b364a79c7da230
Omertis <36013948+Omertis@users.noreply.github.com>
2018-07-30 22:49:31 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.TOKEN = 'NDQ3MTI1NTk1MTQzNDA1NTg5.DeDBrQ.7SyYYfZRj9xWUlyqKr_q5V1zjeY'; // ya wr3 la tns5
+
+exports.PREFIX = 'm';
+
+exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
